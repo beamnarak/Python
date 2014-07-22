5fc093d3472d8587b022f488b3f4aac761f25dae
beamnarak <senserone@gmail.com>
2014-07-22 23:33:25 +0700

    FindFriend

--- findFriend.py
+++ findFriend.py
@@ -3,11 +3,9 @@ def check_connection(network, first, second):
     
     if len(network) > 0:
         for n in network:
-            if str(first) in str(n) and str(second) in str(n): 
-                print(1)
+            if str(first) in str(n) and str(second) in str(n):
                 return True
             elif str(first) in str(n):
-                print(2)
                 r = n.split("-")
                 first = r[1] if r[0] == first else r[0]
                 network = getLid(network, n)
