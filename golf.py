a0dde205e2ad6f39bae7551648c23f4b97c99c99
beamnarak <senserone@gmail.com>
2014-07-27 16:56:18 +0700

    Golf Prime Palindrome [Short]

--- golf.py
+++ golf.py
@@ -1,30 +1,12 @@
-def golf(number):
-    i = number + 1
-    result = i
+def golf(n):
+    r = i = n + 1
     while True:
-        if isPrime(i): and isPalindrome(i):
-            result = i
-            break
-        i += 1
-    return result
-
-def isPrime(number):
-    flag = True
-    for i in range(2,number):
-        if number%i == 0:
-            flag = False
-            break
-
-    return flag
-
-def isPalindrome(number):
-    number = str(number)
-    return number == number[::-1]
-    #flag = True
-    #if len(str(number)) != 1:
-    #    sn = str(number)
-    #    for i in range(int(len(sn)/2)):
-    #        if sn[i] != sn[(i*-1)-1]:
-    #            flag = False
-    #            break
-    #return flag
+        if p(i) and pl(str(i)): r = i; break
+        i+=1
+    return r
+def p(n):
+    for i in range(2,n):
+        if n%i == 0: return False
+    return True
+def pl(n):
+    return n == n[::-1]
